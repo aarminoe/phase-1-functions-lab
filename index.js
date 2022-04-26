7ae0024bac606d0532dade5348493466294b5ec2
Aaron Noe <aarminoe@gmail.com>
2022-04-25 19:40:48 -0500

    commit tree 950d34881970a5b4c02ef50b801a088085fddb30

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const scuberHq = 42
+
+function distanceFromHqInBlocks(distance) {
+    return Math.abs(distance - scuberHq)
+}
+
+function distanceFromHqInFeet(distance) {
+   return distanceFromHqInBlocks(distance) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let blocks = Math.abs(start - destination)
+    return blocks * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    let blocks = Math.abs(start - destination)
+    let distance = blocks * 264
+    let freeComp = distance - 400
+    let flatFee = 25
+    if (distance < 400) {
+        return 0
+    }
+    else if (distance > 400 && distance < 2000) {
+        return freeComp * 0.02
+    }
+    else if (distance > 2000 && distance < 2500) {
+        return flatFee
+    }
+    else if (distance > 2500) {
+        return 'cannot travel that far'
+    }
+}
